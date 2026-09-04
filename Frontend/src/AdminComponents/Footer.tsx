b0d3a6f8da06ca9c5365c05435160b8392c25b0d
Aaloka123 <aalokapoudel10@gmail.com>
2026-09-04 09:38:03 +0545

    feat: update AdminFooter version to v1.6.2

--- Frontend/src/AdminComponents/Footer.tsx
+++ Frontend/src/AdminComponents/Footer.tsx
@@ -18,7 +18,7 @@ import {
 
 const AdminFooter: React.FC = () => {
   const year = new Date().getFullYear();
-  const version = "v1.6.1";
+  const version = "v1.6.2";
 
   const quickLinks = [
     { name: "Dashboard", link: "/admin", icon: LayoutDashboard },
